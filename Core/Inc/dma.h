2e7c3f0007c40ef816d798837d0c7ce451a5e263
Sunshine <emmacharlotteduerresarai@gmail.com>
2023-09-16 21:46:29 +0800

    111

---
+++ Core/Inc/dma.h
@@ -0,0 +1,5 @@
+#ifndef __DMA_H__
+#define __DMA_H__
+#include "stm32f1xx_hal_dma.h"
+
+#endif
---
+++ Core/Src/dma.c
@@ -0,0 +1,28 @@
+//
+// Created by 阳庆瑶 on 2023/9/16.
+//
+#include "dma.h"
+
+const int size = 16;
+const   uint32_t aSRC_Buffer[size]={
+        1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16
+};
+uint32_t dest[size];
+
+void dma_init(){
+    DMA_HandleTypeDef dmaHandleTypeDef;
+    dmaHandleTypeDef.DmaBaseAddress = DMA1;
+    dmaHandleTypeDef.Instance = DMA1_Channel6;
+    DMA_InitTypeDef DMA_Struct = {0};
+    dmaHandleTypeDef.Init = DMA_Struct;
+    DMA_Struct.Direction = DMA_MEMORY_TO_MEMORY;
+    DMA_Struct.PeriphInc = DMA_PINC_ENABLE;
+    DMA_Struct.PeriphDataAlignment = DMA_PDATAALIGN_WORD;
+    DMA_Struct.MemDataAlignment = DMA_PDATAALIGN_WORD;
+    DMA_Struct.MemInc = DMA_PINC_ENABLE;
+    DMA_Struct.Mode = DMA_NORMAL;
+    DMA_Struct.Priority = DMA_PRIORITY_VERY_HIGH;
+    __HAL_RCC_DMA1_CLK_ENABLE();
+
+    HAL_DMA_Start(&dmaHandleTypeDef, (uint32_t) aSRC_Buffer, (uint32_t) dest, size);
+}
